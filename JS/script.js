ca1197a7a5a37ce32d094af68ea121b3ba0e2a6b
PavelLoko88 <118975859+PavelLoko88@users.noreply.github.com>
2023-04-18 23:38:28 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// POPUP
 function popup() {
     let open = document.getElementById('btn-open');
     let close = document.getElementById('btn-close');
@@ -11,3 +12,47 @@ function popup() {
     })
 }
 popup();
+
+
+// TIME
+function time() {
+    let time = document.getElementById('input-time');
+    let compile = document.getElementById('compile');
+    let resetbtn = document.getElementById('reset');
+
+    function startTime() {
+        let result = document.getElementById('result');
+        result.innerText = 'Тут будет конечный результат';
+    }
+
+    function compileTime() {
+
+        let result = document.getElementById('result');
+
+        let second, min;
+        let hours = 0;
+        let day = 0;
+
+        second = Math.floor(time.value % 60);
+        min = Math.floor(time.value / 60);
+        if (min > 60) {
+            hours = Math.floor(min / 60);
+            min = Math.floor(min % 60);
+            if (hours >= 24) {
+                day = Math.floor(hours / 24);
+                hours = Math.floor(hours % 24);
+            }
+        }
+        result.innerText = day + ' дней ' + hours + ' часов ' + min + ' минут ' + second + ' секунд ';
+    }
+
+    function reset() {
+        time.value = '';
+
+        startTime();
+    }
+    startTime();
+    compile.addEventListener("click", compileTime);
+    resetbtn.addEventListener("click", reset);
+}
+time();
